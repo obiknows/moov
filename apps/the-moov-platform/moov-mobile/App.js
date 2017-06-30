cd6b074809ae480721f991a065776243a4e85e67
Sam Nnodim <sam.nnodim@gmail.com>
2017-06-29 17:09:50 -0700

    added /src & /__tests__ & configured the Root <-> App link

--- apps/the-moov-platform/moov-mobile/App.js
+++ apps/the-moov-platform/moov-mobile/App.js
@@ -1,21 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import the Root of the app
+import Root from './src/Root';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Start creating the '/src' directory to  get poppin</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// run the app
+export default Root;
